1b4bc4cf13878bc5949280f9569bbb6b4b4a58ab
Geoff Schmit <gcschmit@gmail.com>
2024-03-07 15:18:10 -0600

    update CANivore bus name

--- java/PhoenixSysId/src/main/java/frc/robot/Constants.java
+++ java/PhoenixSysId/src/main/java/frc/robot/Constants.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
 public class Constants {
-    public final static String CANBUS = "canivore";
-    public final static int TALON_FX_ID = 18;
+    public final static String CANBUS = "canbus1";
+    public final static int TALON_FX_ID = 18; // 21
 }
